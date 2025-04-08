40204050a1b6de82e05a25ba80ab36f0eecc563c
Brijesh Pookkottur <epbrijesh@gmail.com>
2025-04-08 22:08:06 +0530

    Add files via upload

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.app',
+  id: 'com.thinkdigit.swschedule',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
